9f643fffcf7c72f548cf4054d3b7de3b237a4c32
Meha <ahem4progs@gmail.com>
2021-01-26 18:10:08 +0530

    updated vash files

--- server.js
+++ server.js
@@ -4,4 +4,25 @@ var app = express();
 
 app.set('view engine', 'vash');
 app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/index.html', (req, res) => {
+    res.render('index');
+});
+app.get('/about.html', (req, res) => {
+    res.render('about');
+});
+app.get('/services.html', (req, res) => {
+    res.render('services');
+});
+app.get('/blog.html', (req, res) => {
+    res.render('blog');
+});
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+app.get('*', (req, res) => {
+    res.render('index');
+});
 app.listen(8080);
